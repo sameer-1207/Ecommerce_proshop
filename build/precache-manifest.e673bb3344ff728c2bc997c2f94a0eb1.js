298a12f3fd2406365a9f6188b11070da2001815e
Sameer Mohre <73546343+sameer-1207@users.noreply.github.com>
2022-07-01 22:54:50 +0530

    Add files via upload

---
+++ build/precache-manifest.e673bb3344ff728c2bc997c2f94a0eb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5960d03437e52654a2c7411d755f2faf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d119b218cb4b1bd0bc2",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d119b218cb4b1bd0bc2",
+    "url": "/static/js/main.e3cdfbe8.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
